1369b889b22843ab148a1a4b7f22d90eb7a1f42c
Tomas Mankowski <tomy-bsso.16@hotmail.com>
2025-08-23 23:17:53 -0300

    ya casiii

--- src/App.tsx
+++ src/App.tsx
@@ -156,7 +156,7 @@ const normalizeString = (str: string) => {
             className="video-player"
             width="560"
             height="315"
-            src="https://www.youtube.com/embed/qehwr2MMTqg?si=t6roHVYykKbqtAj8?autoplay=1&controls=0&modestbranding=1&rel=0&showinfo=0"
+            src="https://www.youtube.com/embed/dZcjxPbTj40?si=t6roHVYykKbqtAj8?autoplay=1&controls=0&modestbranding=1&rel=0&showinfo=0"
             title="Gender Reveal Video"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
